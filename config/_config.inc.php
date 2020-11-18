c5cf12aefddba400355e22e33fc8cb72869cc0bd
diego <diego.envigo.net>
2020-11-18 22:06:53 +0100

    upgrade config

--- config/_config.inc.php
+++ config/_config.inc.php
@@ -9,37 +9,50 @@
  */
 global $cfg;
 
-$cfg['db_api_token'] = 'X';
+$cfg['db_api_token'] = '';
 $cfg['search_db'] = 'themoviedb';
 $cfg['LANG'] = 'es-ES';
+$cfg['CHARSET'] = 'UTF8';
+$cfg['LOCALE'] = str_replace('-', '_', $cfg['LANG'] .'.'. $cfg['CHARSET']);
 $cfg['cache'] = '/cache';
 $cfg['REL_PATH'] = '/trackerm';
 $cfg['MOVIES_PATH'] = '/home/compartido/biblioteca/Peliculas';
 $cfg['SHOWS_PATH'] = '/home/compartido/biblioteca/Series';
-$cfg['max_identify_items'] = 10;
-$cfg['LANG'] = 'es';
-$cfg['jackett_key'] = 'X';
+$cfg['jacket_results'] = 50;
+$cfg['jackett_key'] = '';
 $cfg['jackett_srv'] = 'http://192.168.X.X:9117';
 $cfg['jackett_indexers'] = [
     0 => 'newpct',
-    1 => 'cinecalidad',
-    2 => 'divxtotal',
-    3 => 'mejortorrent',
-    4 => 'moviesdvdr',
+//    1 => 'divxtotal',
+    2 => 'mejortorrent',
+    3 => 'moviesdvdr',
 //     5 => 'rarbg',
 ];
-$cfg['jacket_results'] = 20;
 
+$cfg['max_identify_items'] = 5;
+$cfg['tresults_rows'] = 2;
+$cfg['tresults_columns'] = 8;
 $cfg['trans_hostname'] = '192.168.X.X';
 $cfg['trans_port'] = '9091';
 $cfg['trans_username'] = '';
 $cfg['trans_passwd'] = '';
-$cfg['tresults_columns'] = 8;
-$cfg['tresults_rows'] = 8;
 $cfg['tresults_details'] = 1;
 $cfg['theme'] = 'default';
+$cfg['CACHE_IMAGES'] = 1;
+$cfg['CACHE_IMAGES_PATH'] = '/cache/images';
 
-/* NOT NEED EDIT */
+$cfg['TORRENT_QUALITYS_PREFS'] = [
+    0 => '720p',
+    1 => '1080p',
+    2 => 'ANY',
+];
+
+$cfg['TORRENT_IGNORES_PREFS'] = [
+    0 => 'LATINO',
+    1 => 'SCREENER',
+];
+
+/* PROBABLY NOT NEED EDIT */
 
 $cfg['jackett_api'] = '/api/v2.0';
 $cfg['img_url'] = $cfg['REL_PATH'] . '/img';
