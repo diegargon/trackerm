bd272f88dcb513dfc9dac3ee79e690f83cb452d2
diego <diego.envigo.net>
2020-11-19 14:06:09 +0100

    lang en

---
+++ lang/en-En/lang.inc.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ *
+ *  @author diego@envigo.net
+ *  @package
+ *  @subpackage
+ *  @copyright Copyright @ 2020 Diego Garcia (diego@envigo.net)
+ */
+global $LNG;
+
+$LNG = [
+    'L_ALL' => 'All',
+    'L_ADDED' => 'Added',
+    'L_ADD' => 'Add',
+    'L_COLUMNS' => 'Columnss',
+    'L_CATEGORY' => 'Categories',
+    'L_DEFAULT' => 'Default',
+    'L_DOWNLOAD' => 'Download',
+    'L_DOWNLOAD_MIN' => 'Download',
+    'L_DB' => $cfg['search_db'],
+    'L_DAY_ALL' => 'Every day',
+    'L_DAY_MON' => 'Monday',
+    'L_DAY_TUE' => 'Thuesday',
+    'L_DAY_WED' => 'Wednesday',
+    'L_DAY_THU' => 'Thursday',
+    'L_DAY_FRI' => 'Friday',
+    'L_DAY_SAT' => 'Saturday',
+    'L_DAY_SUN' => 'Sunday',
+    'L_DELETE' => 'Deleete',
+    'L_ERROR' => 'Error',
+    'L_EPISODES' => 'Episodes',
+    'L_EPISODE' => 'Episode',
+    'L_HOME' => 'Home',
+    'L_IDENT_MOVIES' => 'Identify movies',
+    'L_IDENT_SHOWS' => 'Identify shows',
+    'L_IDENTIFY' => 'Identify',
+    'L_LIBRARY' => 'Library',
+    'L_MOVIES' => 'Movies',
+    'L_MOVIE' => 'Movie',
+    'L_NOID' => 'Unidentifiedr',
+    'L_NOTHING_FOUND' => 'Without results',
+    'L_POPULARITY' => 'Popularity',
+    'L_REBUILD_MOVIES' => 'Rebuild Movies',
+    'L_REBUILD_SHOWS' => 'Rebuild Shows',
+    'L_RELEASE' => 'Release',
+    'L_RATING' => 'Rating',
+    'L_RATING_MIN' => 'R',
+    'L_RESULTS' => 'Results',
+    'L_ROWS' => 'Rows',
+    'L_SHOWS' => 'Shows',
+    'L_SHOW' => 'Show',
+    'L_SIZE' => 'Size',
+    'L_SHOW_TORRENTS' => 'Show Torrents',
+    'L_SEARCH' => 'Search',
+    'L_SEARCH_MOVIES' => 'Search Movies',
+    'L_SEARCH_SHOWS' => 'Search Shows',
+    'L_SEARCHTORRENTS' => 'Torrents',
+    'L_SEARCHTORRENTS_DESC' => 'Meta Torrents Search',
+    'L_SEASON' => 'Season',
+    'L_SEASONS' => 'Seasons',
+    'L_SUBMIT' => 'Submit',
+    'L_TORRENT' => 'Torrent',
+    'L_TORRENTS' => 'Torrents',
+    'L_WANTED' => 'Wanted',
+    'L_WANTED_ADD' => 'Add Wanted',
+    'L_WANT_ALL' => 'Add all',
+];
