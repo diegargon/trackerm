36d197501bf4ead7c8d1cd8b255b87147be86942
diego <diego.envigo.net>
2020-11-20 20:34:58 +0100

    cookies

--- include/session.inc.php
+++ include/session.inc.php
@@ -10,12 +10,20 @@
 session_start();
 
 if (isset($_GET['profile']) && array_key_exists($_GET['profile'], $cfg['profiles'])) {
+
     $_SESSION['profile'] = $_GET['profile'];
+    setcookie("profile", $_SESSION['profile'], time() + 3600000);
+    $cfg['profile'] = $_GET['profile'];
 }
+
+if (!isset($_GET['profile']) && !isset($_SESSION['profile']) && isset($_COOKIE['profile']) && array_key_exists($_COOKIE['profile'], $cfg['profiles'])) {
+    $_SESSION['profile'] = $_COOKIE['profile'];
+    $cfg['profile'] = $_COOKIE['profile'];
+}
+
 if (isset($_SESSION['profile']) && array_key_exists($_SESSION['profile'], $cfg['profiles'])) {
     $cfg['profile'] = $_SESSION['profile'];
 } else {
     $cfg['profile'] = 0;
 }
-
 
