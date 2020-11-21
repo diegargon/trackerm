2f7af7c2d31530e45f336e0adb1ef72e1b1d8462
diego <diego.envigo.net>
2020-11-22 00:00:24 +0100

    transmission 2

--- include/transmission.inc.php
+++ include/transmission.inc.php
@@ -9,17 +9,17 @@
  * https://github.com/transmission/transmission/blob/master/extras/rpc-spec.txt
  */
 function page_transmission() {
-    global $trans;
+    global $trans, $LNG;
 
     $status = [
-        0 => 'Stopped/Paused',
-        1 => 'Queuening for Checking files',
-        2 => 'Checking files',
-        3 => 'Queuening',
-        4 => 'Downloading',
-        5 => 'Queue to seed',
-        6 => 'Seeding',
-        7 => 'No Peers',
+        0 => $LNG['L_STOPPED'],
+        1 => $LNG['L_QUEUENING_CHECKING'],
+        2 => $LNG['L_CHECKING_FILES'],
+        3 => $LNG['L_QUEUENING'],
+        4 => $LNG['L_DOWNLOADING'],
+        5 => $LNG['L_QUEUENING_TO_SEED'],
+        6 => $LNG['L_SEEDING'],
+        7 => $LNG['L_NO_PEERS'],
     ];
 
     $transfers = $trans->getAll();
@@ -31,18 +31,20 @@ function page_transmission() {
         $page .= '<br/>';
         $page .= '<div class="tor_download">';
         $page .= '<div class="tor_name">' . $transfer['name'] . '</div>';
-        $page .= '<div class="tor_downdir">' . $transfer['downloadDir'] . '</div>';
+
         $page .= '</div>'; //tor_download
 
 
         $page .= '<div class="tor_tags">';
         $page .= '<div class="tor_tag"><span>id: ' . $transfer['id'] . '</div>';
-        empty($transfer['isFinished']) ? $isFinished = 0 : $isFinished = $transfer['isFinished'];
-        $page .= '<div class="tor_tag"><span>isFinish: ' . $isFinished . '</div>';
+        //empty($transfer['isFinished']) ? $isFinished = 0 : $isFinished = $transfer['isFinished'];
+        //$page .= '<div class="tor_tag"><span>isFinish: ' . $isFinished . '</div>';
         $transfer['percentDone'] == 1 ? $percentDone = '100' : $percentDone = (float) $transfer['percentDone'];
-        $page .= '<div class="tor_tag"><span>percentDone: ' . $percentDone . '%</div>';
-        $page .= '<div class="tor_tag"><span>status: ' . $status[$transfer['status']] . '</div>';
-        $page .= '</div>';
+        $page .= '<div class="tor_tag"><span>' . $LNG['L_PERCENT'] . ': ' . $percentDone . '%</div>';
+        $page .= '<div class="tor_tag"><span>' . $LNG['L_STATUS'] . ': ' . $status[$transfer['status']] . '</div>';
+        $page .= '<div class="tor_tag">' . $LNG['L_DESTINATION'] . ': ' . $transfer['downloadDir'] . '</div>';
+        $page .= '</div>'; //tor tags
+
         /*
           $page .= '<div class="tor_files">';
           foreach ($transfer['files'] as $file) {
