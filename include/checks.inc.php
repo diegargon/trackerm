28ac5164140e5d7487001315b567c3f5157705b9
diego <diego.envigo.net>
2020-11-27 18:56:33 +0100

    main check, exit if not set

--- include/checks.inc.php
+++ include/checks.inc.php
@@ -13,34 +13,43 @@ function do_checks() {
 
     if (empty($cfg['db_api_token'])) {
         echo "WARNING: You must set in /config/config.inc.php db_api token with your themoviedb api key\n";
+        exit();
     }
     if (empty($cfg['jackett_key'])) {
         echo "WARNING: You must set in /config/config.inc.php jackett_key with your jackett api key\n";
+        exit();
     }
     if (empty($cfg['jackett_srv'])) {
         echo "WARNING: You must set in /config/config.inc.php jackett_srv with you http://ip:port hackett server\n";
+        exit();
     }
     if (empty($cfg['LANG'])) {
-        echo "WARNING: You must set in /config/config.inc.php LANG (only supported es-ES/en-En) \n";
+        echo "WARNING: You must set in /config/config.inc.php LANG (only supported languages es-ES/en-En) \n";
+        exit();
     }
     if (empty($cfg['MOVIES_PATH']) || !is_dir($cfg['MOVIES_PATH'])) {
         echo "WARNING: You must set in /config/config.inc.php MOVIES_PATH where your movies reside \n";
+        exit();
     }
-
     if (empty($cfg['SHOWS_PATH']) || !is_dir($cfg['MOVIES_PATH'])) {
         echo "WARNING: You must set in /config/config.inc.php SHOWS_PATH where your shows reside \n";
+        exit();
     }
     if (empty($cfg['REL_PATH'])) {
         echo "WARNING: You must set in /config/config.inc.php REL_PATH \n";
+        exit();
     }
     if (empty($cfg['TORRENT_FINISH_PATH']) || !is_dir($cfg['TORRENT_FINISH_PATH'])) {
         echo "WARNING: You must set in /config/config.inc.php TORRENT_FINISH_PATH to where torrent put your files (separate from temporal directory) \n";
+        exit();
     }
     if (empty($cfg['ROOT_PATH']) || !is_dir($cfg['ROOT_PATH'])) {
         echo "WARNING: You must set in /config/config.inc.php ROOT_PATH \n";
+        exit();
     }
     if (!is_writable($cfg['ROOT_PATH'] . '/cache')) {
         echo "WARNING: Your cache directory must be writable: {$cfg['ROOT_PATH']}/cache \n";
+        exit();
     }
     if (!is_writable($cfg['TORRENT_FINISH_PATH'])) {
         echo "WARNING: Your \"torrent finish path\" directory must be writable: {$cfg['TORRENT_FINISH_PATH']} \n";
@@ -53,8 +62,10 @@ function do_checks() {
     }
     if (empty($cfg['trans_hostname'])) {
         echo "WARNING: You must set in /config/config.inc.php trans_hostname with the ip of transmission-daemon server ex: \"192.168.1.1\"\n";
+        exit();
     }
     if (empty($cfg['trans_port'])) {
         echo "WARNING: You must set in /config/config.inc.php trans_port with the port of transmission-daemon server ex: \"9091\"\n";
+        exit();
     }
 }
