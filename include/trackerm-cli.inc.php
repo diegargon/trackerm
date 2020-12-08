7cee6cd7ef93153070ac84c346040d228874e964
diego <diego.envigo.net>
2020-12-08 11:18:49 +0100

    check if torrent is a file or directory

--- include/trackerm-cli.inc.php
+++ include/trackerm-cli.inc.php
@@ -36,10 +36,10 @@ function transmission_scan() {
 
             $item['tid'] = $tor['id'];
             $item['hashString'] = $tor['hashString'];
-            $item['dirname'] = $tor['name'];
-            $item['title'] = getFileTitle($item['dirname']);
+            $item['files_location'] = $tor['name'];
+            $item['title'] = getFileTitle($tor['name']);
             $item['status'] = $tor['status'];
-            $item['media_type'] = getMediaType($item['dirname']);
+            $item['media_type'] = getMediaType($tor['name']);
             isset($tor['wanted_id']) ? $item['wanted_id'] = $tor['wanted_id'] : null;
 
             if ($item['media_type'] == 'movies') {
@@ -59,10 +59,10 @@ function transmission_scan() {
 
             $item['tid'] = $tor['id'];
             $item['hashString'] = $tor['hashString'];
-            $item['dirname'] = $tor['name'];
-            $item['title'] = getFileTitle($item['dirname']);
+            $item['files_location'] = $tor['name'];
+            $item['title'] = getFileTitle($tor['name']);
             $item['status'] = $tor['status'];
-            $item['media_type'] = getMediaType($item['dirname']);
+            $item['media_type'] = getMediaType($tor['name']);
             isset($tor['wanted_id']) ? $item['wanted_id'] = $tor['wanted_id'] : null;
 
             if ($item['media_type'] == 'movies') {
@@ -293,50 +293,54 @@ function ShowJob($item, $linked = false) {
 function get_valid_files($item) {
     global $cfg, $LNG, $log;
 
-    $orig_path = $cfg['TORRENT_FINISH_PATH'] . '/' . $item['dirname'];
+    $orig_path = $cfg['TORRENT_FINISH_PATH'] . '/' . $item['files_location'];
 
-    if (!is_dir($orig_path)) {
-        $log->err("$orig_path is not a directory");
-        return false;
-    }
-    $files_dir = scandir_r($orig_path);
+    if (is_dir($orig_path)) {
 
-    foreach ($files_dir as $file) {
-        $ext_check = substr($file, -3);
+        $files_dir = scandir_r($orig_path);
 
-        if ($ext_check == 'rar' || $ext_check == 'RAR') {
-            if (file_exists($cfg['UNRAR_PATH'])) {
-                $unrar_check = dirname($file) . '/trackerm-unrar';
-                if (!file_exists($unrar_check)) {
-                    if (check_file_encrypt('rar', $file)) {
-                        $log->addStateMsg(" {$LNG['L_ERR_FILE_ENCRYPT_MANUAL']} ($file)");
-                        // we continue and try since the function need test and TODO.
+        foreach ($files_dir as $file) {
+            $ext_check = substr($file, -3);
+
+            if ($ext_check == 'rar' || $ext_check == 'RAR') {
+                if (file_exists($cfg['UNRAR_PATH'])) {
+                    $unrar_check = dirname($file) . '/trackerm-unrar';
+                    if (!file_exists($unrar_check)) {
+                        if (check_file_encrypt('rar', $file)) {
+                            $log->addStateMsg(" {$LNG['L_ERR_FILE_ENCRYPT_MANUAL']} ($file)");
+                            // we continue and try since the function need test and TODO.
+                        }
+                        touch($unrar_check);
+                        !empty($cfg['FILES_USERGROUP']) ? chgrp($unrar_check, $cfg['FILES_USERGROUP']) : null;
+                        $unrar = $cfg['UNRAR_PATH'] . ' x -p- -y "' . $file . '" "' . dirname($file) . '"';
+                        $log->info("Need unrar $file");
+                        exec($unrar);
+                        break;
+                    } else {
+                        $log->info("Unrar flag is set skipping");
+                        break;
                     }
-                    touch($unrar_check);
-                    !empty($cfg['FILES_USERGROUP']) ? chgrp($unrar_check, $cfg['FILES_USERGROUP']) : null;
-                    $unrar = $cfg['UNRAR_PATH'] . ' x -p- -y "' . $file . '" "' . dirname($file) . '"';
-                    $log->info("Need unrar $file");
-                    exec($unrar);
-                    break;
                 } else {
-                    $log->info("Unrar flag is set skipping");
-                    break;
+                    $log->addStateMsg($LNG['L_NEED_UNRAR']);
                 }
-            } else {
-                $log->addStateMsg($LNG['L_NEED_UNRAR']);
             }
         }
-    }
 
-    isset($unrar) ? $files_dir = scandir_r($orig_path) : false;
+        isset($unrar) ? $files_dir = scandir_r($orig_path) : false;
 
-    $valid_files = [];
+        $valid_files = [];
 
-    foreach ($files_dir as $file) {
-        if (preg_match($cfg['TORRENT_MEDIA_REGEX'], $file)) {
-            $valid_files[] = $file;
+        foreach ($files_dir as $file) {
+            if (preg_match($cfg['TORRENT_MEDIA_REGEX'], $file)) {
+                $valid_files[] = $file;
+            }
         }
+    } else {
+        $log->err("$orig_path is not a directory (NOT IMPLEMENTED");
+        return false;
     }
+
+    return $valid_files;
 }
 
 function move_media($valid_file, $final_dest_path) {
