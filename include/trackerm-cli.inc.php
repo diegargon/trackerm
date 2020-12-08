d5896919e685296ad38b190230b87b4607952210
diego <diego.envigo.net>
2020-12-08 11:05:02 +0100

    split funcs

--- include/trackerm-cli.inc.php
+++ include/trackerm-cli.inc.php
@@ -136,49 +136,13 @@ function getRightTorrents() {
 }
 
 function MovieJob($item, $linked = false) {
-    global $cfg, $log, $trans, $db, $LNG;
-
-    $orig_path = $cfg['TORRENT_FINISH_PATH'] . '/' . $item['dirname'];
-    $files_dir = scandir_r($orig_path);
-
-    foreach ($files_dir as $file) {
-        $ext_check = substr($file, -3);
-
-        if ($ext_check == 'rar' || $ext_check == 'RAR') {
-            if (file_exists($cfg['UNRAR_PATH'])) {
-                $unrar_check = dirname($file) . '/trackerm-unrar';
-                if (!file_exists($unrar_check)) {
-                    if (check_file_encrypt('rar', $file)) {
-                        $log->addStateMsg(" {$LNG['L_ERR_FILE_ENCRYPT_MANUAL']} ($file)");
-                        //TODO: we continue and try since the function need test and TODO.
-                    }
-                    $unrar = $cfg['UNRAR_PATH'] . ' x -p- -y "' . $file . '" "' . dirname($file) . '"';
-                    touch($unrar_check);
-                    !empty($cfg['FILES_USERGROUP']) ? chgrp($unrar_check, $cfg['FILES_USERGROUP']) : null;
-                    $log->info("Need unrar $file");
-                    exec($unrar);
-                    break;
-                } else {
-                    $log->info("Unrar flag is set skipping");
-                    break;
-                }
-            } else {
-                $log->addStateMsg($LNG['L_NEED_UNRAR']);
-            }
-        }
-    }
-
-    isset($unrar) ? $files_dir = scandir_r($orig_path) : false;
+    global $cfg, $log, $trans, $db;
 
     $valid_files = [];
 
-    foreach ($files_dir as $file) {
-        if (preg_match($cfg['TORRENT_MEDIA_REGEX'], $file)) {
-            $valid_files[] = $file;
-        }
-    }
+    $valid_files = get_valid_files($item);
 
-    if (count($valid_files) >= 1) {
+    if ($valid_files && count($valid_files) >= 1) {
 
         if ($cfg['CREATE_MOVIE_FOLDERS']) {
             $dest_path = $cfg['MOVIES_PATH'] . '/' . ucwords($item['title']);
@@ -240,48 +204,11 @@ function MovieJob($item, $linked = false) {
 function ShowJob($item, $linked = false) {
     global $cfg, $db, $LNG, $trans, $log;
 
-    $orig_path = $cfg['TORRENT_FINISH_PATH'] . '/' . $item['dirname'];
-
-    $files_dir = scandir_r($orig_path);
-
-    foreach ($files_dir as $file) {
-        $ext_check = substr($file, -3);
-        if ($ext_check == 'rar' || $ext_check == 'RAR') {
-            if (file_exists($cfg['UNRAR_PATH'])) {
-                $unrar_check = dirname($file) . '/trackerm-unrar';
-                if (!file_exists($unrar_check)) {
-                    if (check_file_encrypt('rar', $file)) {
-                        $log->addStateMsg(" {$LNG['L_ERR_FILE_ENCRYPT_MANUAL']} ($file)");
-                        // we continue and try since the function need test and TODO.
-                    }
-                    touch($unrar_check);
-                    !empty($cfg['FILES_USERGROUP']) ? chgrp($unrar_check, $cfg['FILES_USERGROUP']) : null;
-                    $unrar = $cfg['UNRAR_PATH'] . ' x -p- -y "' . $file . '" "' . dirname($file) . '"';
-                    $log->info("Need unrar $file");
-                    exec($unrar);
-                    //$log->debug("" . $unrar;
-                    break;
-                } else {
-                    $log->info("Unrar flag is set skipping");
-                    break;
-                }
-            } else {
-                $log->addStateMsg($LNG['L_NEED_UNRAR']);
-            }
-        }
-    }
-
-    isset($unrar) ? $files_dir = scandir_r($orig_path) : false;
-
     $valid_files = [];
 
-    foreach ($files_dir as $file) {
-        if (preg_match($cfg['TORRENT_MEDIA_REGEX'], $file)) {
-            $valid_files[] = $file;
-        }
-    }
+    $valid_files = get_valid_files($item);
 
-    if (count($valid_files) >= 1) {
+    if ($valid_files && count($valid_files) >= 1) {
         $i = 1;
         foreach ($valid_files as $valid_file) {
             $many = '';
@@ -360,6 +287,55 @@ function ShowJob($item, $linked = false) {
         }
     } else {
         $log->info("No valid files found on torrent with {$item['tid']} : {$item['hashString']}");
+    }
+}
+
+function get_valid_files($item) {
+    global $cfg, $LNG, $log;
+
+    $orig_path = $cfg['TORRENT_FINISH_PATH'] . '/' . $item['dirname'];
+
+    if (!is_dir($orig_path)) {
+        $log->err("$orig_path is not a directory");
+        return false;
+    }
+    $files_dir = scandir_r($orig_path);
+
+    foreach ($files_dir as $file) {
+        $ext_check = substr($file, -3);
+
+        if ($ext_check == 'rar' || $ext_check == 'RAR') {
+            if (file_exists($cfg['UNRAR_PATH'])) {
+                $unrar_check = dirname($file) . '/trackerm-unrar';
+                if (!file_exists($unrar_check)) {
+                    if (check_file_encrypt('rar', $file)) {
+                        $log->addStateMsg(" {$LNG['L_ERR_FILE_ENCRYPT_MANUAL']} ($file)");
+                        // we continue and try since the function need test and TODO.
+                    }
+                    touch($unrar_check);
+                    !empty($cfg['FILES_USERGROUP']) ? chgrp($unrar_check, $cfg['FILES_USERGROUP']) : null;
+                    $unrar = $cfg['UNRAR_PATH'] . ' x -p- -y "' . $file . '" "' . dirname($file) . '"';
+                    $log->info("Need unrar $file");
+                    exec($unrar);
+                    break;
+                } else {
+                    $log->info("Unrar flag is set skipping");
+                    break;
+                }
+            } else {
+                $log->addStateMsg($LNG['L_NEED_UNRAR']);
+            }
+        }
+    }
+
+    isset($unrar) ? $files_dir = scandir_r($orig_path) : false;
+
+    $valid_files = [];
+
+    foreach ($files_dir as $file) {
+        if (preg_match($cfg['TORRENT_MEDIA_REGEX'], $file)) {
+            $valid_files[] = $file;
+        }
     }
 }
 
