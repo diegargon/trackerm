cce0b37f5026f5c9bb4c42cff469508daf5acae4
diego <diego.envigo.net>
2020-12-16 00:12:17 +0100

    add jquery

--- tpl/default/html_mstruct.tpl.php
+++ tpl/default/html_mstruct.tpl.php
@@ -22,6 +22,8 @@
                 document.getElementById("loading_wrap").style.display = "block";
             }
         </script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous">
+        </script>
     </head>
 
     <body>
