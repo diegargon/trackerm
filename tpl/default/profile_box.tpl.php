91438b125af9bb22805e4bf51944c76eb95502a6
diego <diego.envigo.net>
2021-01-29 00:13:49 +0100

    design

--- tpl/default/profile_box.tpl.php
+++ tpl/default/profile_box.tpl.php
@@ -11,8 +11,8 @@
 <div class="profile_box">
     <form  method="POST" action="?page=login">
         <button class="login_button" type="submit"><img src="<?= $tdata['REL_PATH'] ?>/img/profile.png" /></button>
-        <div class="profile_name"><input class="login_username" type="text" name="username" value="<?= $tdata['username'] ?>"></div>
-        <div class="profile_password"><input class="login_password" type="password" name="password" value=""/></div>
+        <div class="profile_name"><input size="15" class="login_username" type="text" name="username" value="<?= $tdata['username'] ?>"></div>
+        <div class="profile_password"><input size="15" class="login_password" type="password" name="password" value=""/></div>
     </form>
 </div>
 
