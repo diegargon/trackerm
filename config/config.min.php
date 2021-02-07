03ce2c785bd76908d5c1fe7f28bc39d4a1b8fd71
diego <diego.envigo.net>
2021-02-07 12:50:00 +0100

    spell

--- config/config.min.php
+++ config/config.min.php
@@ -13,6 +13,7 @@
 /* * ******************************************************************************* */
 
 // themoviedb.org api key: https://www.themoviedb.org/documentation/api
+// You must register a account go to "edit profile" and ask for api key
 $cfg['db_api_token'] = '';
 //UI Lang Supported only: es-ES or en-EN
 $cfg['LANG'] = 'en-EN';
@@ -22,13 +23,13 @@ $cfg['LANG'] = 'en-EN';
 //Check themovidedb.org top bar to check languages supported
 //If you upgrade pre-this you must delete trackerm.db and start again since the local registers not going to be upgraded.
 $cfg['TMDB_LANG'] = 'en-EN';
-// Full path where trackerm reside ex: /var/www/html
+// Full path where trackerm reside ex: /var/www/html/trackerm
 $cfg['ROOT_PATH'] = '';
 
 // Relative path (Webserver) where trackerm reside ex: /trackerm for http://mydomain.com/trackerm
 $cfg['REL_PATH'] = '/trackerm';
 
-// Your library path (the first element (0) is where trackerm drop the new content)
+// Your library path (the first element (0) is where trackerm move the new content)
 //Movies
 $cfg['MOVIES_PATH'] = [
     0 => '', //ex: 0 => '/home/mylib/movies',
@@ -37,7 +38,7 @@ $cfg['MOVIES_PATH'] = [
 $cfg['SHOWS_PATH'] = [
     0 => '',
 ];
-// Where transmission put download files (you must separated finish torrents directory  and the temporal file directory)
+// Where transmission put finished download files (you must separated finish torrents directory  and the temporal file directory)
 $cfg['TORRENT_FINISH_PATH'] = '';
 
 //Jacket Server ex: http://192.168.1.1:9117
@@ -45,7 +46,7 @@ $cfg['jackett_srv'] = '';
 //Jacket Key: ex: a long line of characters and number check Jackett
 $cfg['jackett_key'] = '';
 
-// What Jacket indexer will use, check link in "Actions" the NAME ex:  http://192.168.X.XX:9117/api/v2.0/indexers/NAME/results/
+// What Jacket indexer will use, check link in Jacket Web interface (Actions) the NAME ex:  http://192.168.X.XX:9117/api/v2.0/indexers/NAME/results/
 $cfg['jackett_indexers'] = [
 //    1 => 'newpct',
 //    2 => 'divxtotal',
