67ab2c312585d7f14d7b977b3bb493783f59753a
diego <diego.envigo.net>
2021-02-13 12:20:12 +0100

    add check

--- include/checks.inc.php
+++ include/checks.inc.php
@@ -17,31 +17,31 @@ function do_checks() {
         echo "ERROR: trackerm ins't configure please copy & rename config/config.min.php to /etc/trackerm.conf and fill all fields\n";
     }
     if (empty($cfg['db_api_token'])) {
-        echo "ERROR: You must set in /config/config.inc.php db_api token with your themoviedb api key\n";
+        echo "ERROR: You must set in /etc/trackrm.conf db_api_token with your themoviedb api key\n";
         exit();
     }
     if (empty($cfg['jackett_key'])) {
-        echo "ERROR: You must set in /config/config.inc.php jackett_key with your jackett api key\n";
+        echo "ERROR: You must set in /etc/trackrm.conf  jackett_key with your jackett api key\n";
         exit();
     }
     if (empty($cfg['jackett_srv'])) {
-        echo "ERROR: You must set in /config/config.inc.php jackett_srv with you http://ip:port hackett server\n";
+        echo "ERROR: You must set in /etc/trackrm.conf jackett_srv with you http://ip:port hackett server\n";
         exit();
     }
     if (empty($cfg['LANG'])) {
-        echo "ERROR: You must set in /config/config.inc.php LANG (only supported languages es-ES/en-En) \n";
+        echo "ERROR: You must set in /etc/trackrm.conf  LANG (only supported languages es-ES/en-En) \n";
         exit();
     }
     if (empty($cfg['REL_PATH'])) {
-        echo "ERROR: You must set in /config/config.inc.php REL_PATH \n";
+        echo "ERROR: You must set in /etc/trackrm.conf  REL_PATH \n";
         exit();
     }
     if (empty($cfg['TORRENT_FINISH_PATH']) || !is_dir($cfg['TORRENT_FINISH_PATH'])) {
-        echo "ERROR: You must set in /config/config.inc.php TORRENT_FINISH_PATH to where torrent put your files (separate from temporal directory) \n";
+        echo "ERROR: You must set in /etc/trackrm.conf  TORRENT_FINISH_PATH to where torrent put your files (separate from temporal directory) \n";
         exit();
     }
     if (empty($cfg['ROOT_PATH']) || !is_dir($cfg['ROOT_PATH'])) {
-        echo "ERROR: You must set in /config/config.inc.php ROOT_PATH \n";
+        echo "ERROR: You must set in /etc/trackrm.conf p ROOT_PATH \n";
         exit();
     }
     if (!is_writable($cfg['ROOT_PATH'] . '/cache')) {
@@ -57,14 +57,30 @@ function do_checks() {
         exit();
     }
     if (!is_writable($cfg['TORRENT_FINISH_PATH'])) {
-        echo "ERROR: Your \"torrent finish path\" directory must be writable: {$cfg['TORRENT_FINISH_PATH']} \n";
+        echo "WARNING: Your \"torrent finish path\" directory must be writable: {$cfg['TORRENT_FINISH_PATH']} \n";
     }
     if (!is_array($cfg['MOVIES_PATH']) && !is_writable($cfg['MOVIES_PATH'])) {
-        echo "ERROR: Your \"MOVIES_PATH \" directory must be writable: {$cfg['MOVIES_PATH']} \n";
+        echo "WARNING: Your \"MOVIES_PATH \" directory must be writable: {$cfg['MOVIES_PATH']} \n";
     }
     if (!is_array($cfg['MOVIES_PATH']) && !is_writable($cfg['SHOWS_PATH'])) {
-        echo "ERROR: Your \"SHOWS_PATH \" directory must be writable: {$cfg['SHOWS_PATH'] }\n";
+        echo "WARNING: Your \"SHOWS_PATH \" directory must be writable: {$cfg['SHOWS_PATH'] }\n";
     }
+
+    if (is_array($cfg['MOVIES_PATH'])) {
+        foreach ($cfg['MOVIES_PATH'] as $mp_key => $movies_path) {
+            if (!is_writable($movies_path)) {
+                echo "WARNING: Your {$movies_path} directory must be writable: {$cfg['MOVIES_PATH'][$mp_key]} \n";
+            }
+        }
+    }
+    if (is_array($cfg['SHOWS_PATH'])) {
+        foreach ($cfg['SHOWS_PATH'] as $sp_key => $shows_path) {
+            if (!is_writable($shows_path)) {
+                echo "WARNING: Your {$shows_path} directory must be writable: {$cfg['SHOWS_PATH'][$sp_key]} \n";
+            }
+        }
+    }
+
     if (empty($cfg['trans_hostname'])) {
         echo "ERROR: You must set in /config/config.inc.php trans_hostname with the ip of transmission-daemon server ex: \"192.168.1.1\"\n";
         exit();
