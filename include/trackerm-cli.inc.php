ebb0f9f7e57d65c8b16c6b6b3342b28c2d0682c1
diego <diego.envigo.net>
2021-02-26 22:24:44 +0100

    fix wrong table

--- include/trackerm-cli.inc.php
+++ include/trackerm-cli.inc.php
@@ -856,7 +856,7 @@ function update_trailers() {
 
     $log->debug('Executing update trailers');
     $limit = 15;
-    $tables = ['library_movies', 'library_master_shows', 'tmdb_search_movies', 'tmdb_search_shows'];
+    $tables = ['library_movies', 'library_shows', 'tmdb_search_movies', 'tmdb_search_shows'];
     //IMPROVE: This can be done in one query per table
     // Update missing trailers and never try get trailer
     foreach ($tables as $table) {
