9365b31851107080b0e4776b86f4c72d31f2566f
diego <diego.envigo.net>
2021-06-18 20:01:26 +0200

    fix sort

--- includes/library.inc.php
+++ includes/library.inc.php
@@ -42,10 +42,10 @@ function show_my_movies() {
 
     if (empty($search_term)) {
         $topt['num_table_rows'] = $db->qSingle("SELECT COUNT(*) FROM library_master_movies WHERE title <> ''");
-        $query = "SELECT * FROM library_master_movies WHERE title <> '' ORDER BY created DESC LIMIT $start,$end ";
+        $query = "SELECT * FROM library_master_movies WHERE title <> '' ORDER BY updated DESC LIMIT $start,$end ";
     } else {
         $topt['num_table_rows'] = $db->qSingle("SELECT COUNT(*) FROM library_master_movies WHERE title <> '' AND title LIKE \"%$search_term%\" ");
-        $query = "SELECT * FROM library_master_movies WHERE title <> '' AND title LIKE \"%$search_term%\" ORDER BY created DESC LIMIT $start,$end ";
+        $query = "SELECT * FROM library_master_movies WHERE title <> '' AND title LIKE \"%$search_term%\" ORDER BY updated DESC LIMIT $start,$end ";
     }
 
     $results = $db->query($query);
@@ -97,10 +97,10 @@ function show_my_shows() {
 
     if (empty($search_term)) {
         $topt['num_table_rows'] = $db->qSingle("SELECT COUNT(*) FROM library_master_shows WHERE title <> ''");
-        $query = "SELECT * FROM library_master_shows WHERE title <> '' ORDER BY created DESC LIMIT $start,$end ";
+        $query = "SELECT * FROM library_master_shows WHERE title <> '' ORDER BY updated DESC LIMIT $start,$end ";
     } else {
         $topt['num_table_rows'] = $db->qSingle("SELECT COUNT(*) FROM library_master_shows WHERE title <> '' AND title LIKE \"%$search_term%\" ");
-        $query = "SELECT * FROM library_master_shows WHERE title <> '' AND title LIKE \"%$search_term%\" ORDER BY created DESC LIMIT $start,$end ";
+        $query = "SELECT * FROM library_master_shows WHERE title <> '' AND title LIKE \"%$search_term%\" ORDER BY updated DESC LIMIT $start,$end ";
     }
     $results = $db->query($query);
     $shows = $db->fetchAll($results);
