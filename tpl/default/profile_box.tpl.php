86537a5493b4be61a41209d43ba5007ccf28804b
diego <diego.envigo.net>
2021-07-12 20:53:10 +0200

    minor

--- tpl/default/profile_box.tpl.php
+++ tpl/default/profile_box.tpl.php
@@ -10,7 +10,7 @@
 
 <div class="profile_box">
     <form  method="POST" action="?page=login">
-        <button class="login_button" type="submit"><img src="<?= $cfg['REL_PATH'] ?>/img/profile.png" /></button>
+        <button class="login_button" type="submit"><img alt="" src="<?= $cfg['REL_PATH'] ?>/img/profile.png" /></button>
         <div class="profile_name"><input size="15"  onfocus="this.value = ''" class="login_username" type="text" name="username" value="<?= $tdata['username'] ?>"></div>
         <div class="profile_password"><input size="15"  onfocus="this.value = ''" class="login_password" type="password" name="password" value=""/></div>
     </form>
