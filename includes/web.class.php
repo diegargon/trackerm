256da8488e3cb310f49adc0ce61f2302eb98ba35
diego <diego.envigo.net>
2021-07-27 00:11:30 +0200

    collect

--- includes/web.class.php
+++ includes/web.class.php
@@ -41,7 +41,7 @@ class Web {
                 exit();
             }
         }
-        $valid_pages = ['index', 'library', 'news', 'tmdb', 'torrents', 'view', 'wanted', 'identify',
+        $valid_pages = ['index', 'library', 'news', 'tmdb', 'torrents', 'view', 'view_group', 'wanted', 'identify',
             'download', 'localplayer', 'identify', 'download', 'transmission', 'config', 'login', 'logout'];
 
         (!isset($req_page) || $req_page == '') ? $req_page = 'index' : null;
