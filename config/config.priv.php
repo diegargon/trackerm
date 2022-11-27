c7bb279bd368b1561a74309b0203f476e2f612df
diego <diego.envigo.net>
2022-11-27 12:02:27 +0100

    up

--- config/config.priv.php
+++ config/config.priv.php
@@ -13,7 +13,7 @@
 !defined('IN_WEB') ? exit : true;
 
 $cfg['search_db'] = 'themoviedb';
-$cfg['version'] = 'A100';
+$cfg['version'] = 'A101';
 $cfg['odb_movies_link'] = 'https://www.themoviedb.org/movie/';
 $cfg['odb_shows_link'] = 'https://www.themoviedb.org/tv/';
 $cfg['odb_images_link'] = 'https://image.tmdb.org/t/p/w500';
